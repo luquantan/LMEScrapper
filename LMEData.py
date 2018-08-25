d680f814197352a18af12b5c4b4bc0e885c5bd4b
Wei Yiing <wyquek@gmail.com>
2018-08-25 16:35:30 +0800

    Refactored code into functions

--- LMEData.py
+++ LMEData.py
@@ -1,37 +1,60 @@
 import pandas as pd
 import pdb
+from bs4 import BeautifulSoup # For HTML parsing
 
-html = BeautifulSoup(raw_html, 'html.parser')
+def parseHTMLResponse(raw_html):
+    return BeautifulSoup(raw_html, 'html.parser')
 
-# Get Table Object
-table = html.find("table") # defaults to the first table
+def getHTMLTable(html):
+    return html.find("table") # defaults to the first table
 
-## Get Table Headers
-data_header = []
-table_head = table.find('thead')
-rows = table_head.find_all('tr')
+def getTableHeader(table):
+    data_header = []
+    table_head = table.find('thead')
+    rows = table_head.find_all('tr')
+    for row in rows:
+        cols = row.find_all('th')
+        cols = [ele.text.strip() for ele in cols]
+        data_header.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data_header
 
-for row in rows:
-    cols = row.find_all('th')
-    cols = [ele.text.strip() for ele in cols]
-    data_header.append([ele for ele in cols if ele]) # Get rid of empty values
+def getTableBody(table):
+    data = []
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
 
+def convertListToDataFrame(data_header, data):
+    # Store Data as a Data Frame
+    df = pd.DataFrame(data, columns=data_header[0])
+    df.set_index('Contract', inplace = True)
+    return df
 
-# Get Table Body
-data = []
-table_body = table.find('tbody')
-rows = table_body.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele]) # Get rid of empty values
+def getColumnName(priceType):
+    if priceType == 'bid':
+        return 'Bid (US$ / Tonne)'
+    elif priceType == 'offer':
+        return 'Offer (US$ / Tonne)*'
 
+def getRowName(contractType):
+    if contractType == 'cash':
+        return 'Cash'
+    elif contractType == '3month':
+        return '3-months'
 
-# Store Data as a Data Frame
-df = pd.DataFrame(data, columns=data_header[0])
-df.set_index('Contract', inplace = True)
+def retrieveData(raw_html, priceType, contractType):
+    html = parseHTMLResponse(raw_html)
+    table = getHTMLTable(html)
+    data_header = getTableHeader(table)
+    data = getTableBody(table)
+    df = convertListToDataFrame(data_header, data)
+    colName = getColumnName(priceType)
+    rowName = getRowName(contractType)
+    return df[colName][rowName]
 
-# Retrieve Bid Price
-print df['Bid (US$ / Tonne)']['Cash']
-
-print "done"
+def retrieveBidPriceCashFor(raw_html):
+    return retrieveData(raw_html, 'bid', 'cash')
