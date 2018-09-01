93a56b30092a6217357d9a4f1a77665c57285e12
Wei Yiing <wyquek@gmail.com>
2018-09-01 14:34:21 +0800

    WIP Export Date to CSV

--- LMEScrapperProject.py
+++ LMEScrapperProject.py
@@ -1,5 +1,6 @@
 import pdb
 import pprint
+import pandas as pd
 
 import LMENetworkManager as NetworkManager
 import LMEData
@@ -25,5 +26,26 @@ def alumHTMLResponse():
 # Find out what date this data is valid for
 
 # Find out what is the current closing price 
-print "Copper price is: " + str(LMEData.retrieveBidPriceCashFor(copperHTMLResponse()))
-print "Alum price is: " + str(LMEData.retrieveBidPriceCashFor(alumHTMLResponse()))
+# print "Copper price is: " + str(LMEData.retrieveBidPriceCashFor(copperHTMLResponse()))
+# print "Alum price is: " + str(LMEData.retrieveBidPriceCashFor(alumHTMLResponse()))
+
+copperData = LMEData.retrieveBidPriceCashFor(copperHTMLResponse())
+alumData = LMEData.retrieveBidPriceCashFor(alumHTMLResponse())
+
+copperStrings = ("Copper Date", "Copper Bid Price")
+alumStrings = ("Alum Date", "Alum Bid Price")
+
+copperDataList = list((copperStrings, copperData))
+alumDataList = list((alumStrings, alumData))
+
+copperDataFrame = pd.DataFrame(copperDataList)
+alumDataFrame = pd.DataFrame(alumDataList)
+
+copperDataFrame.to_csv('copper.csv', index=False)
+alumDataFrame.to_csv('alum.csv', index=False)
+
+import pdb
+pdb.set_trace()
+
+
+print "done"
